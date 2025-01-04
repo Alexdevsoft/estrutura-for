829b5fb70f55a5543df7597c71eab38b59a5093b
Alexdevsoft <alexhavilla2022@gmail.com>
2025-01-03 22:41:44 -0300

    soma

--- src/estrutura_for/Main.java
+++ src/estrutura_for/Main.java
@@ -8,7 +8,17 @@ public class Main {
 
 		Scanner scanner = new Scanner(System.in);
 		
+		int N = scanner.nextInt();
 		
+		int soma = 0;
+		for(int i=0; i<N; i++) {
+			int x = scanner.nextInt();
+			soma = soma + x;
+			
+		}
+		System.out.println(soma);
+		
+		scanner.close();
 
 	}
 
